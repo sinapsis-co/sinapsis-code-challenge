65201b70a802b7dfa73ba5288839249e2802b837
julian <julian.merlo@naranja.com>
2020-08-14 16:19:18 -0300

    fix: modify index and package

--- thumbnail-generator-ui/src/utils/Index.js
+++ thumbnail-generator-ui/src/utils/Index.js
@@ -1,3 +1,5 @@
+const { v4: uuid } = require("uuid");
+
 function previewImage(event) {
   let reader = new FileReader();
   let file = event[0];
@@ -9,7 +11,10 @@ function previewImage(event) {
       let div = document.createElement("div");
       let image = document.createElement("img");
       image.src = reader.result;
+      file.data = reader.result;
       image.className = "loadimage__preview__image";
+      image.id = uuid();
+      file.id = image.id;
       div.id = "preview_images";
       div.className = "preview_images";
       preview.append(div);
@@ -24,9 +29,9 @@ function previewImage(event) {
 function estructureObject(images) {
   const imageData = images.map((image) => {
     return (estructureObject = {
+      path: image.data,
       size: image.size,
       type: image.type,
-      path: image.path,
     });
   });
   return imageData;
