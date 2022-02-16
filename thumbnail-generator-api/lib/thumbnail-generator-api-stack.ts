8a85e9d1d5f2273e4ee7d688580c2a6ea6518db9
Alan Gatticelli <alan.gatticelli@mercadolibre.com>
2022-02-15 23:40:13 -0300

    Add bucket creation

--- thumbnail-generator-api/lib/thumbnail-generator-api-stack.ts
+++ thumbnail-generator-api/lib/thumbnail-generator-api-stack.ts
@@ -1,16 +1,12 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
 export class ThumbnailGeneratorApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ThumbnailGeneratorApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Bucket(this, 'ThumbnailBucket', {
+      bucketName: 'thumbnails-store',
+    });
   }
 }
