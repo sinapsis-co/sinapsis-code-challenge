6e05d5fc22704ad7f6e6dc3a2dd957c3f0910c59
Amelia Neville <muzikgirl12@gmail.com>
2022-09-28 13:31:30 -0500

    second commit

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ const app = express();
 //define port
 const port = 8080;
 
+
 //routes
 app.get('/', (req, res) => {
-  res.send("listening successfully!");
+  res.send('listening successfully!');
 });
+
+app.get('/thumbnails', (req, res) => {
+  res.send('these are your thumbnails');
+});
+
+//connect to db
+
 
 //listen to the server
 app.listen(port);
