7822e994bdf327947029368d01a8ef8b1208c91f
Amelia Neville <muzikgirl12@gmail.com>
2022-09-29 20:38:33 -0500

    pushing changes from branch to main

--- thumbnail-generator-api/routes/posts.js
+++ thumbnail-generator-api/routes/posts.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 
 const postRoutes = Router();
 
-
+//get post put delete
 postRoutes.get('/posts', (req, res) => {
   res.send('Hello from the posts page!');
 });
