079788428eb4a1693719e98a2bc30fd71c2ba63c
Amelia Neville <muzikgirl12@gmail.com>
2022-09-29 20:03:34 -0500

    adding routes

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-//define axios
-const axios = require('axios');
 //define express
 const express =  require('express');
+//define mongoose 
+const mongoose = require('mongoose');
+//dotenv
+require('dotenv/config');
 //define app
 const app = express();
 //define port
 const port = 8080;
 
+//import routes
+const postsRoutes = require('./thumbnail-generator-api/routes/posts');
+
 //routes
-//get, put, post, delete
 app.get('/', (req, res) => {
-  res.send("listening successfully!");
+  res.send('Hello from the homepage!');
 });
 
+app.use('/posts', postsRoutes);
+
+
+//connect to db
+mongoose.connect(process.env.DB_CONNECTION, () => console.log('connected to DB'));
+
 //listen to the server
-app.listen(port);
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
